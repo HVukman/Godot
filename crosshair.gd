3ce3689cd8b0750209b9e12692ef71e445b59ef4
HVukman <peterslopp@googlemail.com>
2023-06-09 15:13:40 +0200

    Update crosshair.gd

--- crosshair.gd
+++ crosshair.gd
@@ -10,19 +10,10 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	
-	pass
-
 func _input(event: InputEvent) -> void:
-	#if event.is_action_pressed(&"ui_cancel"):
-	#	get_tree().quit() # Quits the game
-	
 	var tween:Tween
-	
-	
 	var spread=crosshair_material.get_shader_parameter("spread")
+	
 	if event.is_action_pressed("shoot"):
 		print("hit")
 		tween=get_tree().create_tween()
@@ -30,8 +21,5 @@ func _input(event: InputEvent) -> void:
 		tween.tween_property(crosshair_material,"shader_parameter/spread",spread,0.5)
 		await(1.0)
 		tween.tween_property(crosshair_material,"shader_parameter/spread",0.0,0.9)
-		#crosshair_material.set_shader_parameter("spread",spread)
-		
-		#crosshair_material.shader
-		pass
+	
 		
